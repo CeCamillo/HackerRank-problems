151e13a88f3f2769761c7d1f7bcb078e58e7c494
CeCamillo <cesarcamillo2212@gmail.com>
2022-08-30 16:46:57 -0300

    Few Exercises

---
+++ src/main/java/JavaProblems/JavaOutputFormatting.java
@@ -0,0 +1,23 @@
+package JavaProblems;
+
+import java.util.Scanner;
+
+public class JavaOutputFormatting {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String s1 =sc.next();
+            int x =sc.nextInt();
+            //Complete this line
+
+            if (s1.length() > 10 || x < 0 || x > 999) {
+                break;
+            }
+            else {
+                System.out.printf("%-14s %03d\n", s1, x);
+            }
+        }
+        System.out.println("================================");
+    }
+}
